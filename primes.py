3eb940b8a28c571d8928aa3fe382a04c86436400
Viola Harperath <viola.harperath@kcl.ac.uk>
2022-10-01 17:46:34 +0100

    First attempt at returning n number of primes with exceptions

--- primes.py
+++ primes.py
@@ -3,4 +3,18 @@
 
 def primes(number_of_primes):
     list = []
+    try:
+        if(number_of_primes <= 0):
+            raise ValueError("Number of primes can not 0 or negative")
+    except ValueError:
+            raise ValueError
+    current_value = 2
+    while len(list) < number_of_primes:
+        for i in range(2, current_value+1):
+            if(current_value == i):
+                list.append(i)
+                current_value +=1
+            elif(current_value%i ==0):
+                current_value +=1
+                break
     return list
